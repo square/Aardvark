48a19c26bc3c648d340518ee59ffb002c329bf8a
Nick Entin <entin@squareup.com>
2020-08-23 00:10:18 -0700

    Restore the log distribution queue's QoS before calling completion handler

--- CoreAardvark/ARKLogDistributor.m
+++ CoreAardvark/ARKLogDistributor.m
@@ -184,8 +184,8 @@
 {
     [self _setDistributionQualityOfServiceUserInitiated];
     [self.logDistributingQueue addOperationWithBlock:^{
-        [[NSOperationQueue mainQueue] addOperationWithBlock:completionHandler];
         [self _setDistributionQualityOfServiceBackground];
+        [[NSOperationQueue mainQueue] addOperationWithBlock:completionHandler];
     }];
 }
 
