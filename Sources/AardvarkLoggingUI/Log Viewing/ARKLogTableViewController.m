a4c57eaa49004d77667092100515f09d6387e1c0
Justin Steffen <JustinDSN@gmail.com>
2020-12-16 15:04:46 -0800

    Update Sources/AardvarkLoggingUI/Log Viewing/ARKLogTableViewController.m
    Co-authored-by: Nick Entin <nckentn@gmail.com>

--- Sources/AardvarkLoggingUI/Log Viewing/ARKLogTableViewController.m
+++ Sources/AardvarkLoggingUI/Log Viewing/ARKLogTableViewController.m
@@ -352,7 +352,7 @@ typedef NS_ENUM(NSUInteger, ARKLogExportOption) {
 #endif
 }
 
-///Exports logs to specified destination. Using enum allows for a single method for exporting and avoided duplication of code.
+/// Exports logs to specified destination. Using enum allows for a single method for exporting and avoided duplication of code.
 - (void)_exportLogsWithOption:(ARKLogExportOption)option;
 {
     #if TARGET_IPHONE_SIMULATOR
