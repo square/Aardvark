4fd0b89ede4ea7c675312ba21bcadceb4127c6d1
Nick Entin <entin@squareup.com>
2021-02-15 01:06:41 -0800

    Remove Swift variadic method

--- Sources/CoreAardvark/Logging/ARKLogDistributor+SwiftAdditions.swift
+++ Sources/CoreAardvark/Logging/ARKLogDistributor+SwiftAdditions.swift
@@ -37,26 +37,4 @@ extension ARKLogDistributor {
         log(withText: text, image: image, type: type, parameters: parameters, userInfo: userInfo)
     }
 
-    /// Creates a log message and distributes it to the log observers.
-    ///
-    /// - parameter format: The format string for the text of the log message.
-    /// - parameter arguments: The arguments to apply to the format string.
-    /// - parameter type: The type of log message.
-    /// - parameter image: An optional image to be attached to the log message. Typically used for messages of type
-    /// `.screenshot`.
-    /// - parameter parameters: A set of key/value pairs that is persisted with the log message.
-    /// - parameter userInfo: A set of key/value pairs that is _not_ persisted with the log message, but rather can be
-    /// used to control how the message is processed by various log observers.
-    public func log(
-        format: String,
-        _ arguments: CVarArg...,
-        type: ARKLogType = .default,
-        image: UIImage? = nil,
-        parameters: [String: String] = [:],
-        userInfo: [AnyHashable: Any]? = nil
-    ) {
-        let text = String(format: format, arguments: arguments)
-        log(text, type: type, image: image, parameters: parameters, userInfo: userInfo)
-    }
-
 }
